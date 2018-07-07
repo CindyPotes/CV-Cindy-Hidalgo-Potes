ed442c6ae07b56545ee4e46d02ade2341f735ed9
CindyPotes <cindyjhidalgo@hotmail.com>
2018-07-07 19:31:52 -0300

    primera version ok

--- cv.js
+++ cv.js
@@ -1,15 +1,8 @@
 $('img').fadeOut()
 $('.imagen').fadeOut()
-$('.cuerpo').fadeOut()
-$('.texto').fadeOut()
 
 $(document).ready(function() {
   $(".nombre").addClass('nombre1')
   $('img').fadeIn(1500)
   $('.imagen').fadeIn(2500)
-  $('.cuerpo').fadeIn()
-});
-
-$(document).scroll(function() {
-	$('.texto').fadeIn(5000)
 });
